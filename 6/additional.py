71d5a886c87e2a00e79b8c1a526d57be003ff1af
YaKalmar0 <49203144+YaKalmar0@users.noreply.github.com>
2019-04-15 22:00:26 +0300

    Update additional.py

--- 6/additional.py
+++ 6/additional.py
@@ -1,10 +1,23 @@
 def func(count, *strings):
+    if count <= 0:
+        return("Error")
     strn = []
     for i in strings:
+        print("NEW KEY")
         str0 = []
         for j in i:
             key = ord(j)
-            sym = chr(key + count)
+            print("key1 = ", key)
+            k = 0
+            while(k < count):
+                key += 1
+                print("key2 = ", key)
+                if key is 123:
+                    key = 92
+                k += 1
+                print("key3 = ", key)
+                
+            sym = chr(key)
             str0 += sym
             str0 = [''.join(str0)]
         strn += str0
@@ -15,4 +28,4 @@ def func(count, *strings):
 str1 = 'abc'
 str2 = 'dklt'
 
-print(func(1, str1, str2))
+print(func(12, str1, str2))
